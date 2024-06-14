fefdddc5aed9c1226db1855796dc35dcce11e6fa
«Julia» <ovcharenkojulia05@gmail.com>
2024-06-14 16:00:29 +0300

    first

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,34 @@
-  
+const burger = document.querySelector('.btn-burger');
+const navigation = document.querySelector('.nav_list')
+
+const toggleMenu =()=> {
+burger.classList.toggle('open');
+navigation.classList.toggle('show');
+}
+
+
+burger.addEventListener('click', toggleMenu)
+
+
+
+
+
+Fancybox.bind("[data-fancybox]", {});
+
+$('.single-item').slick();
+
+
+const swiper = new Swiper('.slider_info', {  
+  spaceBetween: 40,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: false,
+})
+;
